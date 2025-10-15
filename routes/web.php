569b704b81ab7e83c4971c57f86a921adefd5fb2
defani <defani24si@mahasiswa.pcr.ac.id>
2025-10-15 11:15:24 +0700

    pertemuan 6

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PelangganController;
+
+
 
 
 Route::get('/', function () {
@@ -33,4 +36,6 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
+
+Route::resource('pelanggan', PelangganController::class);
 
